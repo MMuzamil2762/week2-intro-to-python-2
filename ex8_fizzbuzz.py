e095c4ec92b39d81ffc7bf247da70cf1d8550e99
MMuzamil2762 <61705583+MMuzamil2762@users.noreply.github.com>
2024-10-13 11:21:57 +0100

    Update ex8_fizzbuzz.py

--- ex8_fizzbuzz.py
+++ ex8_fizzbuzz.py
@@ -1,2 +1,12 @@
 def fizzbuzz(n):
-   pass
+   res = []
+   for i in range(1,n+1):
+      if i%3==0 and i%5==0:
+         res.append('FizzBuzz')
+      elif i %3==0:
+         res.append('Fizz')
+      elif i%5==0:
+         res.append('Buzz')
+      else:
+         res.append(i)
+   return res
