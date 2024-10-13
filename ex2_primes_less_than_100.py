756b0be7e260bcc284c155603d53fd10a02e2fd1
MMuzamil2762 <61705583+MMuzamil2762@users.noreply.github.com>
2024-10-13 11:16:17 +0100

    Update ex2_primes_less_than_100.py

--- ex2_primes_less_than_100.py
+++ ex2_primes_less_than_100.py
@@ -1,2 +1,9 @@
+from ex1_is_prime import is_prime
+
 def primes_less_than_100():
-    pass
+    prime_lst = []
+    for i in range(2, 100):
+        if is_prime(i):
+            prime_lst.append(i)
+
+    return prime_lst
