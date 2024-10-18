cc9a3a1796333511699a43668b6ad6d3f367ce01
MMuzamil2762 <61705583+MMuzamil2762@users.noreply.github.com>
2024-10-18 06:19:24 +0100

    Solution

--- ex1_is_prime.py
+++ ex1_is_prime.py
@@ -1,2 +1,8 @@
 def is_prime(n):
-    pass
+    if n <= 1:
+        return False
+    for j in range(2, int(n)):
+        if n % j == 0:
+            return False
+    return True
+
--- ex3_first_100_primes.py
+++ ex3_first_100_primes.py
@@ -1,2 +1,13 @@
+from ex1_is_prime import is_prime
+
 def first_100_primes():
-    pass
+    first_100_pr = []
+    n = 2
+    while len(first_100_pr) < 100:
+        if is_prime(n):
+            first_100_pr.append(n)
+
+        n += 1
+
+    return first_100_pr
+
